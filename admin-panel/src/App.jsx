d83d0abab5329767d9eb245651bbfba5adb0c16e
aviatricksaero-lab <aviatricksaerolab@gmail.com>
2026-04-07 14:11:07 +0530

    access1

--- admin-panel/src/App.jsx
+++ admin-panel/src/App.jsx
@@ -36,7 +36,6 @@ import {
 } from '@mui/icons-material';
 import { BrowserRouter as Router, Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-
 // Components
 import UsersTable from './components/UsersTable';
 import SessionsTable from './components/SessionsTable';
